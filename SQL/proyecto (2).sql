a1ee75188032942647e388326faf11af35e7f78d
Scarlette <shokkadin@gmail.com>
2020-05-23 23:00:21 -0400

    Sql Proyecto

--- SQL/proyecto (2).sql
+++ SQL/proyecto (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-03-2020 a las 19:07:46
+-- Tiempo de generación: 24-05-2020 a las 04:58:19
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.3
 
@@ -21,6 +21,29 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyecto`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id_alumn` int(11) NOT NULL,
+  `nombres` varchar(200) NOT NULL,
+  `celular` varchar(80) NOT NULL,
+  `email` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id_alumn`, `nombres`, `celular`, `email`) VALUES
+(2, 'Flores Gutierrez', '00-98277-0', 'mimail2@dominio2.com'),
+(3, 'Mena Garcia, Maria Fernanda', '00-985654', 'mi-correo2@mi-dominio.com'),
+(4, 'Flores Figueroa, Luis Armando', '00-985654', 'floresfilu@mi-dominio.com'),
+(5, 'juan cerna', '932', 'shoka@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -53,22 +76,24 @@ INSERT INTO `chat` (`id`, `nombre`, `mensaje`, `fecha`) VALUES
 
 CREATE TABLE `cliente` (
   `id_cliente` int(11) NOT NULL,
-  `rut` varchar(9) DEFAULT NULL,
+  `rut` varchar(10) DEFAULT NULL,
   `nombres` varchar(30) DEFAULT NULL,
   `apellidos` varchar(30) DEFAULT NULL,
   `telefono` int(9) DEFAULT NULL,
   `direccion` varchar(50) DEFAULT NULL,
   `email_cl` varchar(50) DEFAULT NULL,
-  `fecha_ingreso` datetime DEFAULT NULL
+  `fecha_ingreso` datetime DEFAULT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
-INSERT INTO `cliente` (`id_cliente`, `rut`, `nombres`, `apellidos`, `telefono`, `direccion`, `email_cl`, `fecha_ingreso`) VALUES
-(1, '17183287k', 'juan roberto', 'cerna escobar', 932606932, 'Torres del paine 746', 'shokkadin@gmail.com', '2019-12-10 06:33:22'),
-(2, '177103322', 'Ruben ', 'Cerna', 655655, 'Nivdad 8747', 'rfcerna@gmail.com', '2020-01-08 17:06:02');
+INSERT INTO `cliente` (`id_cliente`, `rut`, `nombres`, `apellidos`, `telefono`, `direccion`, `email_cl`, `fecha_ingreso`, `estado`) VALUES
+(1, '17183287k', 'juan roberto', 'cerna escobar', 932606932, 'Torres del paine 746', 'shokkadin@gmail.com', '2019-12-10 06:33:22', 1),
+(2, '177103322', 'Ruben ', 'Cerna', 655655, 'Nivdad 8747', 'rfcerna@gmail.com', '2020-01-08 17:06:02', 1),
+(5, '21788808-5', 'Carlos Eduardo', 'Nova Telgie', 54555545, 'Torres del paine', 'eduard@gmail.com', '2020-03-27 18:32:56', 1);
 
 -- --------------------------------------------------------
 
@@ -89,7 +114,9 @@ CREATE TABLE `detalle` (
 --
 
 INSERT INTO `detalle` (`id_detalle`, `id`, `id_vehiculo`, `detalle`, `fecha_detalle`) VALUES
-(1, 11, 1, 'VEHICULO SE ENCUENTRA EN PERFECTO ESTADO TIENE QUE VOLVER EN LOS PROXIMOS 10000 KILOMETROS :D\n\n-Falta Aceite y varias cosas mas :D\n-Cambio pastillas de freno', '2020-01-29 16:17:52');
+(1, 11, 4, 'VEHICULO SE ENCUENTRA EN PERFECTO ESTADO TIENE QUE VOLVER EN LOS PROXIMOS 10000 KILOMETROS :D\n\n-Falta Aceite y varias cosas mas :D\n-Cambio pastillas de freno', '2020-01-29 16:17:52'),
+(2, 10, 2, 'nada', '2020-04-13 17:51:21'),
+(3, 14, 4, 'Transmision rota', '2020-04-13 17:57:02');
 
 -- --------------------------------------------------------
 
@@ -119,9 +146,11 @@ INSERT INTO `ficha_tecnica` (`id_ficha`, `id_vehiculo`, `id_producto`, `estado_v
 
 CREATE TABLE `mis_productos` (
   `id` int(11) NOT NULL,
+  `Barra` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `price` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `Stock` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
@@ -131,8 +160,10 @@ CREATE TABLE `mis_productos` (
 -- Volcado de datos para la tabla `mis_productos`
 --
 
-INSERT INTO `mis_productos` (`id`, `name`, `description`, `price`, `created`, `modified`, `status`) VALUES
-(5, 'Filtros', 'Camioneta', '5000', '2020-03-20 13:24:55', '2020-03-21 13:24:55', '1');
+INSERT INTO `mis_productos` (`id`, `Barra`, `name`, `description`, `price`, `Stock`, `created`, `modified`, `status`) VALUES
+(1, '7802900638016', '', '', '', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '1'),
+(2, '7802810008893', '', '', '', 0, '2020-05-23 23:03:47', '0000-00-00 00:00:00', '1'),
+(8, '4719867213190', 'Gotita', 'PEGAMENTO', '5000', 50, '2020-05-23 23:14:09', '0000-00-00 00:00:00', '1');
 
 -- --------------------------------------------------------
 
@@ -154,10 +185,9 @@ CREATE TABLE `orden` (
 --
 
 INSERT INTO `orden` (`id`, `customer_id`, `total_price`, `created`, `modified`, `status`) VALUES
-(6, 1, 10000, '2020-03-20 11:34:30', '2020-03-20 11:34:30', '1'),
-(7, 1, 15000, '2020-03-20 11:40:04', '2020-03-20 11:40:04', '1'),
-(8, 1, 15000, '2020-03-20 11:42:06', '2020-03-20 11:42:06', '1'),
-(9, 1, 25000, '2020-03-20 12:57:39', '2020-03-20 12:57:39', '1');
+(2, 2, 5500, '2020-03-30 06:36:00', '2020-03-30 06:36:00', '1'),
+(3, 1, 11500, '2020-03-30 06:41:17', '2020-03-30 06:41:17', '1'),
+(4, 1, 10500, '2020-03-30 22:33:41', '2020-03-30 22:33:41', '1');
 
 -- --------------------------------------------------------
 
@@ -177,10 +207,12 @@ CREATE TABLE `orden_articulos` (
 --
 
 INSERT INTO `orden_articulos` (`id`, `order_id`, `product_id`, `quantity`) VALUES
-(1, 6, 5, 2),
-(2, 7, 5, 3),
-(3, 8, 5, 3),
-(4, 9, 5, 5);
+(2, 2, 6, 1),
+(3, 2, 5, 1),
+(4, 3, 6, 3),
+(5, 3, 5, 2),
+(6, 4, 6, 1),
+(7, 4, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -207,6 +239,30 @@ INSERT INTO `productos` (`id_producto`, `nombre_producto`, `descripcion_prod`, `
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id_pro` int(11) NOT NULL,
+  `rut` varchar(11) DEFAULT NULL,
+  `empresa` varchar(50) DEFAULT NULL,
+  `vendedor` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `direccion` varchar(40) DEFAULT NULL,
+  `ingreso_fecha` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`id_pro`, `rut`, `empresa`, `vendedor`, `email`, `telefono`, `direccion`, `ingreso_fecha`) VALUES
+(1, '17183287-K', '', 'Marcelo sanhueza fernandez', 'cybermastome@gmail.com', 2147483647, 'Egaña 1231 Tomé', '2020-05-24 00:49:04');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `reserva`
 --
 
@@ -214,10 +270,24 @@ CREATE TABLE `reserva` (
   `id_reserva` int(11) NOT NULL,
   `id_cliente` int(11) DEFAULT NULL,
   `id_vehiculo` int(11) DEFAULT NULL,
-  `fecha_reserv` date DEFAULT NULL,
+  `fecha_reserv` varchar(60) NOT NULL,
   `Hora` varchar(50) NOT NULL,
-  `estado_reserva` varchar(60) DEFAULT NULL
+  `estado_reserva` int(1) DEFAULT 1,
+  `ingreso_fecha` varchar(30) NOT NULL,
+  `tipo` varchar(30) NOT NULL DEFAULT 'Sucursal'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `reserva`
+--
+
+INSERT INTO `reserva` (`id_reserva`, `id_cliente`, `id_vehiculo`, `fecha_reserv`, `Hora`, `estado_reserva`, `ingreso_fecha`, `tipo`) VALUES
+(1, 1, 1, '07-04-2020', '10:00', 2, '2020-04-08 02:01:56', 'Sucursal'),
+(2, 2, 4, '08-04-2020', '10:00', 2, '2020-04-08 02:02:04', 'Sucursal'),
+(3, 1, 2, '09-04-2020', '10:00', 3, '2020-04-08 02:02:18', 'Sucursal'),
+(4, 1, 3, '17-04-2020', '13:00', 1, '2020-04-08 02:32:29', 'Sucursal'),
+(5, 2, 4, '13-04-2020', '10:00', 2, '2020-04-13 21:14:47', 'Sucursal'),
+(6, 1, 2, '13-04-2020', '12:00', 2, '2020-04-13 21:16:14', 'Sucursal');
 
 -- --------------------------------------------------------
 
@@ -289,6 +359,12 @@ CREATE TABLE `venta` (
 --
 
 --
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id_alumn`);
+
+--
 -- Indices de la tabla `chat`
 --
 ALTER TABLE `chat`
@@ -343,6 +419,12 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`);
 
 --
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_pro`);
+
+--
 -- Indices de la tabla `reserva`
 --
 ALTER TABLE `reserva`
@@ -377,6 +459,12 @@ ALTER TABLE `venta`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id_alumn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de la tabla `chat`
 --
 ALTER TABLE `chat`
@@ -386,13 +474,13 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle`
 --
 ALTER TABLE `detalle`
-  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `ficha_tecnica`
@@ -404,19 +492,19 @@ ALTER TABLE `ficha_tecnica`
 -- AUTO_INCREMENT de la tabla `mis_productos`
 --
 ALTER TABLE `mis_productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `orden`
 --
 ALTER TABLE `orden`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `orden_articulos`
 --
 ALTER TABLE `orden_articulos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -425,10 +513,16 @@ ALTER TABLE `productos`
   MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id_pro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
